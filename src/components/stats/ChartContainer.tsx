8b3826833e3b15a32603010eef2d41b4bf7e5f50
Dustin Nguyen <dustin7212@gmail.com>
2022-04-16 19:02:20 -0500

    Remove chart table

--- src/components/stats/ChartContainer.tsx
+++ src/components/stats/ChartContainer.tsx
@@ -10,8 +10,6 @@ import {
   faAngleUp,
   faAngleDown,
 } from "@fortawesome/free-solid-svg-icons";
-import Table from "../Table";
-import { Disclosure, Transition } from "@headlessui/react";
 
 type Props = {
   width?: number | string;
@@ -33,86 +31,59 @@ export default function ChartContainer({
   const [ykey, setYKey] = useState("count");
 
   return (
-    <Disclosure>
-      {({ open }) => (
-        <>
-          <div className="my-5 rounded-lg bg-gray-100 px-7">
-            {type === "line" && (
-              <LineChart
-                width={width ?? "100%"}
-                height={height ?? 300}
-                data={data}
-                keys={{ x: xKey, y: ykey }}
-              />
-            )}
-            {type === "bar" && (
-              <BarChart
-                width={height ?? "100%"}
-                height={width ?? 300}
-                data={data}
-                keys={{ x: xKey, y: ykey }}
-              />
-            )}
-            {type === "pie" && (
-              <PieChart
-                width={height ?? "100%"}
-                height={width ?? 300}
-                data={data}
-                keys={{ x: xKey, y: ykey }}
-              />
-            )}
-            <div className="flex items-center justify-between px-3 py-5">
-              <h1 className="text-2xl font-bold">{name}</h1>
-              <div className="flex divide-x divide-gray-500">
-                <div className="pr-1">
-                  <Button
-                    onClick={() => setYKey("count")}
-                    size="sm"
-                    icon={faArrowDown91}
-                    text="Count"
-                    active={ykey === "count"}
-                  />
-                  <Button
-                    onClick={() => setYKey("time_watched")}
-                    size="sm"
-                    icon={faClock}
-                    text="Time Watched"
-                    active={ykey === "time_watched"}
-                  />
-                  <Button
-                    onClick={() => setYKey("mean_score")}
-                    size="sm"
-                    icon={faDivide}
-                    text="Mean Score"
-                    active={ykey === "mean_score"}
-                  />
-                </div>
-                <div className="pl-1">
-                  <Disclosure.Button>
-                    <Button
-                      size="sm"
-                      icon={open ? faAngleUp : faAngleDown}
-                      text={open ? "Hide Table" : "Show Table"}
-                    />
-                  </Disclosure.Button>
-                </div>
-              </div>
-            </div>
-            <Transition
-              enter="transition-all ease-in duration-200"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-all ease-out duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Disclosure.Panel>
-                <Table data={data} dataIndex={xKey} />
-              </Disclosure.Panel>
-            </Transition>
-          </div>
-        </>
+    <div className="my-5 rounded-lg bg-gray-100 px-7">
+      {type === "line" && (
+        <LineChart
+          width={width ?? "100%"}
+          height={height ?? 300}
+          data={data}
+          keys={{ x: xKey, y: ykey }}
+        />
       )}
-    </Disclosure>
+      {type === "bar" && (
+        <BarChart
+          width={height ?? "100%"}
+          height={width ?? 300}
+          data={data}
+          keys={{ x: xKey, y: ykey }}
+        />
+      )}
+      {type === "pie" && (
+        <PieChart
+          width={height ?? "100%"}
+          height={width ?? 300}
+          data={data}
+          keys={{ x: xKey, y: ykey }}
+        />
+      )}
+      <div className="flex items-center justify-between px-3 py-5">
+        <h1 className="text-2xl font-bold">{name}</h1>
+        <div className="flex divide-x divide-gray-500">
+          <div className="pr-1">
+            <Button
+              onClick={() => setYKey("count")}
+              size="sm"
+              icon={faArrowDown91}
+              text="Count"
+              active={ykey === "count"}
+            />
+            <Button
+              onClick={() => setYKey("time_watched")}
+              size="sm"
+              icon={faClock}
+              text="Time Watched"
+              active={ykey === "time_watched"}
+            />
+            <Button
+              onClick={() => setYKey("mean_score")}
+              size="sm"
+              icon={faDivide}
+              text="Mean Score"
+              active={ykey === "mean_score"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
