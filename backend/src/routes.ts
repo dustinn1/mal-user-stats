5753ad1bf667c03eabed9d11cbaa198eef346b6e
Dustin Nguyen <dustin7212@gmail.com>
2022-05-16 23:20:25 -0500

    Separate generate routes into new, status, and fetch

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,7 @@
 import type { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
+import { readFileSync } from "fs";
 import { getStats } from "./stats";
+import { fileExists } from "./utils/fileExists";
 
 export default async function routes(fastify: FastifyInstance) {
   fastify.get("*", async (_request: FastifyRequest, reply: FastifyReply) => {
@@ -17,7 +19,7 @@ export default async function routes(fastify: FastifyInstance) {
   };
 
   fastify.post(
-    "/generate",
+    "/generate/new",
     { schema: generateBodyJsonschema },
     async (
       request: FastifyRequest<{
@@ -25,7 +27,57 @@ export default async function routes(fastify: FastifyInstance) {
       }>,
       reply: FastifyReply
     ) => {
-      return reply.send(await getStats(request.body.username, "anime"));
+      await getStats(request.body.username);
+      return reply.code(200).send("success");
+    }
+  );
+
+  fastify.post(
+    "/generate/status",
+    { schema: generateBodyJsonschema },
+    async (
+      request: FastifyRequest<{
+        Body: { username: string };
+      }>,
+      reply: FastifyReply
+    ) => {
+      if (
+        await fileExists(
+          "src/temp_data/" + request.body.username + "_anime.json"
+        )
+      ) {
+        reply.code(200).send("stats has been generated");
+      } else {
+        reply.code(404).send("stats has not been generated");
+      }
+    }
+  );
+
+  fastify.post(
+    "/generate/fetch",
+    { schema: generateBodyJsonschema },
+    async (
+      request: FastifyRequest<{
+        Body: { username: string; type: string };
+      }>,
+      reply: FastifyReply
+    ) => {
+      if (
+        await fileExists(
+          "src/temp_data/" + request.body.username + "_anime.json"
+        )
+      ) {
+        reply.send(
+          JSON.parse(
+            readFileSync(
+              "src/temp_data/" + request.body.username + "_anime.json",
+              "utf-8"
+            )
+          )
+        );
+      } else {
+        reply.code(404).send("stats has not been generated");
+      }
     }
   );
 }
