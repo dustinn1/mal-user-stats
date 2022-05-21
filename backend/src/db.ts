76d3d1b65a4f063ab58539ca861ed41832ddf860
Dustin Nguyen <dustin7212@gmail.com>
2022-05-21 00:05:27 -0500

    Add some protection against stats generating too often

---
+++ backend/src/db.ts
@@ -0,0 +1,5 @@
+import Keyv from "keyv";
+
+export const currentlyGenerating = new Set();
+
+export const generatedStats = new Keyv();
--- backend/src/stats/index.ts
+++ backend/src/stats/index.ts
@@ -2,17 +2,22 @@ import { fetchFullList } from "./fetch";
 import { generateAnimeStats } from "./anime";
 import type { AnimeStats } from "../interfaces/animeStats";
 import { writeFileSync } from "fs";
+import { currentlyGenerating, generatedStats } from "../db";
 
 export async function getStats(username: string): Promise<AnimeStats | null> {
   try {
-    writeFileSync(
-      "src/temp_data/" + username + "_anime.json",
-      JSON.stringify(
-        generateAnimeStats(await fetchFullList(username, "anime"))
-      ),
-      "utf8"
-    );
-    //return generateAnimeStats(await fetchFullList(username, "anime"));
+    if (!currentlyGenerating.has(username)) {
+      currentlyGenerating.add(username);
+      writeFileSync(
+        "src/temp_data/" + username + "_anime.json",
+        JSON.stringify(
+          generateAnimeStats(await fetchFullList(username, "anime"))
+        ),
+        "utf8"
+      );
+      generatedStats.set(username, "", 300000); // 5 minutes
+      currentlyGenerating.delete(username);
+    }
   } catch (error) {
     console.error(error);
   }
