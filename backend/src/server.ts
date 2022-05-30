36b1ff76ac0b1b043935c299ff20672b58ccc2e9
Dustin Nguyen <dustin7212@gmail.com>
2022-05-30 14:23:44 -0500

    Fix backend not launching on heroku

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,17 +2,17 @@ import Fastify from "fastify";
 import routes from "./routes";
 import cors from "@fastify/cors";
 
-const fastify = Fastify();
+const fastify = Fastify({ logger: true });
 
 fastify.register(cors, {
   origin: ["https://mal-user-stats.vercel.app", "http://localhost:3000"],
 });
 fastify.register(routes);
 
-fastify.listen(process.env["PORT"] || 8000, function (err, address) {
+fastify.listen(process.env["PORT"] || 8000, "0.0.0.0", function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-  console.log(`Server is now listening on ${address}`);
+  fastify.log.info(`Server is now listening on ${address}`);
 });
